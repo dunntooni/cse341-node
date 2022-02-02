02f938a08cce38666b8c40e767b17e39bb643627
dunntooni <dun15014@byui.edu>
2022-02-01 18:07:33 -0700

    Fully functioning heroku site question mark???

--- nodejs-guide/app.js
+++ nodejs-guide/app.js
@@ -3,11 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
-require('dotenv').config();
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
+const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 const app = express();
 
@@ -20,14 +20,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+   User.findById('61f36494a9f4191d40c4fbab')
+      .then((user) => {
+         req.user = user;
+         next();
+      })
+      .catch((err) => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,9 +36,22 @@ app.use(errorController.get404);
 
 mongoose
    .connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6osm.mongodb.net/shop?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6osm.mongodb.net/shop?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
    )
    .then((result) => {
+      User.findOne().then((user) => {
+         if (!user) {
+            const user = new User({
+               name: 'Isaac',
+               email: 'isaac@test.com',
+               cart: {
+                  items: [],
+               },
+            });
+            user.save();
+         }
+      });
       app.listen(PORT);
    })
    .catch((err) => {
