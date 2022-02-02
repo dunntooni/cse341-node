0b3d733182b7b328ac7ff63d9eb9d0553ec86c6d
dunntooni <dun15014@byui.edu>
2022-02-01 21:51:39 -0700

    First half of week 5 reading done

--- nodejs-guide/app.js
+++ nodejs-guide/app.js
@@ -3,25 +3,44 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6osm.mongodb.net/shop`;
 
 const app = express();
+const store = new MongoDBStore({
+   uri: MONGODB_URI,
+   collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+   session({
+      secret: 'my secret',
+      resave: false,
+      saveUninitialized: false,
+      store: store,
+   })
+);
 
 app.use((req, res, next) => {
-   User.findById('61f36494a9f4191d40c4fbab')
+   if (!req.session.user) {
+      return next();
+   }
+   User.findById(req.session.user._id)
       .then((user) => {
          req.user = user;
          next();
@@ -31,14 +50,15 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-   .connect(
-      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j6osm.mongodb.net/shop?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-   )
+   .connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
    .then((result) => {
       User.findOne().then((user) => {
          if (!user) {
