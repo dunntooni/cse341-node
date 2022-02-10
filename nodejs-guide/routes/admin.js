6abce37fe3619502e37b52e61465a6e3eb19940b
dunntooni <dun15014@byui.edu>
2022-02-09 22:51:08 -0700

    Finally finished week 6 reading!!

--- nodejs-guide/routes/admin.js
+++ nodejs-guide/routes/admin.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const { body } = require('express-validator/check');
 
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
@@ -14,7 +15,17 @@ router.get('/add-product', isAuth, adminController.getAddProduct); // These fiel
 router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post(
+   '/add-product',
+   [
+      body('title').isString().isLength({ min: 3 }).trim(),
+      body('imageUrl').isURL(),
+      body('price').isFloat(),
+      body('description').isLength({ min: 3, max: 400 }).trim(),
+   ],
+   isAuth,
+   adminController.postAddProduct
+);
 
 router.get(
    '/edit-product/:productId',
@@ -22,7 +33,17 @@ router.get(
    adminController.getEditProduct
 );
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post(
+   '/edit-product',
+   [
+      body('title').isString().isLength({ min: 3 }).trim(),
+      body('imageUrl').isURL(),
+      body('price').isFloat(),
+      body('description').isLength({ min: 3, max: 400 }).trim(),
+   ],
+   isAuth,
+   adminController.postEditProduct
+);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
